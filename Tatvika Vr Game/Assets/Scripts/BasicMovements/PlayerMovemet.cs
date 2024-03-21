515772ca7275b32ee74329a449c6be1c4ff67add
JeevanJaison <jeevanjaisonjmj@gmail.com>
2024-03-21 20:43:04 +0530

    Created player movement with mouse and keyboarf

---
+++ Tatvika Vr Game/Assets/Scripts/BasicMovements/PlayerMovemet.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovemet : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void FixedUpdate()
+    {
+        movePlayer();
+    }
+
+    private void Update()
+    {
+        myInput();
+    }
+
+    //The function to read the user keyboard input
+    private void myInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    void movePlayer()
+    {
+        //Calculate the movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+}
